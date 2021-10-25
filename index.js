b22123f4adfbcf82b13c35664f7fbb6dc38d8d7f
PSK-9 <67982679+PSK-9@users.noreply.github.com>
2021-10-25 16:13:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ app.get('/', (req, res) => {
 
   let htmlString = "<ol> <b>testfargate</b>";
   const AWS = require("aws-sdk")
-//   var s3 = new AWS.S3({apiVersion: '2006-03-01',accessKeyId:"AKIAXOCGFUCAMJUO5IV4",secretAccessKey:"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"});
+  var s3 = new AWS.S3({apiVersion: '2006-03-01',accessKeyId:"AKIAXOCGFUCAMJUO5IV4",secretAccessKey:"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"});
 
-//   s3.listObjects({Bucket: "simplesb"}, function(err, data) {
-//     if (err) {
-//       console.log(err, err.stack);
-//       res.send(err);
-//     }
-//     else {
-//             for(var data of data.Contents)              
-//                 htmlString += `<li>${data.Key}</li>`;
-//             htmlString+="</ol>";
-//             console.log(htmlString);
-//         }
-//     res.send(htmlString);
-//   });
-  res.send("Hello");
+  s3.listObjects({Bucket: "simplesb"}, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      res.send(err);
+    }
+    else {
+            for(var data of data.Contents)              
+                htmlString += `<li>${data.Key}</li>`;
+            htmlString+="</ol>";
+            console.log(htmlString);
+        }
+    res.send(htmlString);
+  });
+//  res.send("Hello");
 
 })
 
